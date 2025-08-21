cf5f972315d7b8d770dfe84bfec1a9be093465ad
haklesh051 <hakleshbairwa051@gmail.com>
2025-08-21 22:20:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "27730810"
+api_hash = "dadc82d593e3f5334da107b63ced36b9"
+bot_token = "8394698016:AAEdlL4JlS7TXYvq4md_n6Bmig0tPxtfIm8"
+auth_users = [6132794263]
